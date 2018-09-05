b84efb8e891f51eb00cfeaddf39b6577a56032af
Steven Faulkner <sfaulkner1@gmail.com>
2018-09-05 01:37:51 -0400

    added project 1

---
+++ Assignment1-1/Assignment1-1/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+ *  Steven Faulkner 
+ *  U96161844 Assignment 1-1
+ *  Small OOP project to get familiar with c#
+ */
+
+using System;
+using System.Collections.Generic;
+
+
+namespace Assignment1_1
+{
+    class Student
+    {
+        private string first_name;
+        private string last_name;
+        private string ID;
+        private string email;
+        private int age;
+        private string returningStudent;
+
+        //loop will gather all info then pass to the constructor, only need the getters to return info, and no setters since 
+        // we are creating an object after all data has been passed.
+        public Student(string First_Name, string Last_Name, string id, string Email, int Age, string ReturningStudent)
+        {
+            first_name = First_Name;
+            last_name = Last_Name;
+            ID = id;
+            email = Email;
+            age = Age;
+            returningStudent = ReturningStudent;
+        }
+
+        public string GetFirst() => first_name;
+        public string GetLast() => last_name;
+        public string GetID() => ID;
+        public string GetEmail() => email;
+        public int GetAge() => age;
+        public string GetStatus() => returningStudent;
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            List<Student> ListOfStudents = new List<Student>();
+
+            bool DontStop = true;
+
+
+            //gather user objects until break
+            while (DontStop) 
+            {
+
+                Console.Write("Enter first name: ");
+                string fName = Console.ReadLine();
+
+                Console.Write("Enter last name: ");
+                string lName = Console.ReadLine();
+
+                Console.Write("Enter University ID: ");
+                string Uid = Console.ReadLine();
+
+                Console.Write("Enter email address: ");
+                string Email = Console.ReadLine();
+
+                Console.Write("Enter your age: ");
+                int Age = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("New Student?");
+                string rtrnStudent = Console.ReadLine();
+
+                ListOfStudents.Add(new Student(fName,lName,Uid,Email,Age,rtrnStudent));
+            
+                Console.Write("Enter another student(Yes or No)? ");
+                string loopBreak = Console.ReadLine();
+
+                DontStop = (string.Equals(loopBreak, b: "yes", comparisonType: StringComparison.OrdinalIgnoreCase)) ? true : false; 
+
+            }
+
+            int count = 1;
+
+        //iterate through each element of object list 
+        foreach(Student tmpStudent in ListOfStudents)
+            {
+                Console.WriteLine("\nStudent # " + count++);
+                Console.WriteLine("Student Name: " + tmpStudent.GetFirst() + " " + tmpStudent.GetLast());
+                Console.WriteLine("Student ID: " + tmpStudent.GetID());
+                Console.WriteLine("Student Email: " + tmpStudent.GetEmail());
+                Console.WriteLine("Student Age: " + tmpStudent.GetAge());
+                Console.WriteLine("Returning Student: " + tmpStudent.GetStatus());
+            }
+
+            ListOfStudents.Clear();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment1-2/Assignment1-2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ *  Steven Faulkner 
+ *  U96161844 Assignment 1-2
+ *  Small OOP project to get familiar with c#
+ */
+namespace Assignment1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> IntList = new List<int>();
+
+            int input;
+
+            Console.Write("Enter any natural number: ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            //if first number is 0 we just exit
+            if (input != 0) {
+
+                IntList.Add(input);
+
+                while (input != 0)
+                {
+                    Console.Write("Enter any natural number: ");
+                    input = Convert.ToInt32(Console.ReadLine());
+                    IntList.Add(input);
+                }
+
+                //remove the zero at the end of the list
+                IntList.RemoveAt(IntList.Count - 1);
+
+                int sum = 0;
+                int max = IntList[0];
+                int min = IntList[0];
+                int lastElement = IntList.Count - 1;
+                int firstElement = 0;
+
+                //check for min and max while having to sum the elements
+                foreach (int num in IntList)
+                {
+                    max = (max > num) ? max : num;
+                    min = (min < num) ? min : num;
+                    sum += num;
+                }
+
+
+                //output results
+                Console.WriteLine("Sum of array elements: " + sum);
+                Console.WriteLine("Minimum element in array: " + min);
+                Console.WriteLine("Maximum element of arry: " + max);
+                Console.WriteLine("Quotient: " + IntList[firstElement] / IntList[lastElement]);
+                Console.WriteLine("Remainder: " + IntList[firstElement] % IntList[lastElement]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
